adbfb197108aae8e6c443c549f6965f5eb46c577
Jack <giacomo.deluca@hotmail.it>
2025-04-13 12:17:34 +0200

    test with documents

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: 'https://www.sanskritvoyager.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/guide/',
+  baseUrl: '/documents/',
   
 
   // GitHub pages deployment config.
